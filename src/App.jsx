3c0932b69049b54d73890919e248a1d3965969ab
ariaan ahmed <you@example.com>
2023-05-06 19:27:19 +0600

    sending post api to server

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,46 @@ const App = () => {
     })
   }, [])
 
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email);
+    
+    const user = {name, email};
+    
+    console.log(user);
+
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    
+  }
+
+
   return (
     <>
       <h2>user management system</h2>
-      <h5>{users.length}</h5>
+      <h2>User: {users.length}</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="name" /> <br />
+        <input type="email" name="email" placeholder="email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+
+
+      <div>
+        {
+          users.map((user) => <p key={user.id}>{user.id}. {user.name} - {user.email}</p>)
+        }
+      </div>
     </>
   );
 };
